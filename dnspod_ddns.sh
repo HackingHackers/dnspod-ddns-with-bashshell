004d43502a5727596e5eafaa1d34466d7eeb07ef
Andy Han <52874216+HackingHackers@users.noreply.github.com>
2022-11-18 14:24:35 -0500

    Refine log format

--- dnspod_ddns.sh
+++ dnspod_ddns.sh
@@ -9,25 +9,25 @@ CHECKURL="https://myip4.ipip.net"
 # OUT="pppoe"
 # CONF END
 
-date
+printf "[$(date +"%F %T %Z")] "
 if (echo $CHECKURL | grep -q "://"); then
 	IPREX='([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
 	URLIP=$(curl -4 -k $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s $CHECKURL | grep -Eo "$IPREX" | tail -n1)
 	if (echo $URLIP | grep -qEvo "$IPREX"); then
-		URLIP="Get $DOMAIN URLIP failed."
+		URLIP="Failed to query IP."
 	fi
-	echo "[URL IP]:$URLIP"
+	# echo "[URL IP]: $URLIP"
 	dnscmd="nslookup"
 	type nslookup >/dev/null 2>&1 || dnscmd="ping -c1"
 	DNSTEST=$($dnscmd $host.$domain)
 	if [ "$?" != 0 ] && [ "$dnscmd" == "nslookup" ] || (echo $DNSTEST | grep -qEvo "$IPREX"); then
-		DNSIP="Get $host.$domain DNS failed."
+		DNSIP="Failed to reach DNS."
 	else
 		DNSIP=$(echo $DNSTEST | grep -Eo "$IPREX" | tail -n1)
 	fi
-	echo "[DNS IP]:$DNSIP"
+	# echo "[DNS IP]: $DNSIP"
 	if [ "$DNSIP" == "$URLIP" ]; then
-		echo "IP hasn't changed, skipping."
+		printf "SKIPPING: IP not changed\n"
 		exit
 	fi
 fi
@@ -37,19 +37,19 @@ Record="$(curl -4 -k $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s -X
 iferr="$(echo ${Record#*code} | cut -d'"' -f3)"
 if [ "$iferr" == "1" ]; then
 	record_ip=$(echo ${Record#*value} | cut -d'"' -f3)
-	echo "[API IP]:$record_ip"
+	# echo "[API IP]: $record_ip"
 	if [ "$record_ip" == "$URLIP" ]; then
-		echo "IP has already been updated, skipping."
+		printf "SKIPPING: already updated\n"
 		exit
 	fi
 	record_id=$(echo ${Record#*\"records\"\:\[\{\"id\"} | cut -d'"' -f2)
 	record_line_id=$(echo ${Record#*line_id} | cut -d'"' -f3)
-	echo Start DDNS update...
+	# echo Start DDNS update...
 	ddns="$(curl -4 -k $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s -X POST https://dnsapi.cn/Record.Ddns -d "${token}&record_id=${record_id}&record_line_id=${record_line_id}")"
 	ddns_result="$(echo ${ddns#*message\"} | cut -d'"' -f2)"
-	printf "DDNS update result: $ddns_result "
+	printf "$ddns_result: $record_ip -> "
 	echo $ddns | grep -Eo "$IPREX" | tail -n1
 else
-	printf "Get $host.$domain error: "
+	printf "ERROR: "
 	echo $(echo ${Record#*message\"}) | cut -d'"' -f2
 fi
